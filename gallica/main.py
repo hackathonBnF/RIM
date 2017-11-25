7b443dcecefe4c4d4cebf410582517583b801671
Nicolas Bounoughaz <nicolas.bounoughaz@scmfrance.fr>
2017-11-25 19:16:21 +0100

    store partition

--- gallica/main.py
+++ gallica/main.py
@@ -2,6 +2,8 @@
 
 import requests
 import re
+from random import randint
+import json
 
 url_gallica = "http://gallica.bnf.fr/"
 
@@ -12,11 +14,27 @@ def get_xml_gallica():
 
 def get_ark(xml):
     ark = []
+    xml = xml.split("\n")
     for line in xml:
-        match_obj = re.search(r'<link>(.*)<[/]link>', xml)
+        match_obj = re.search(r'<dc:identifier>http://gallica.bnf.fr/(.*)<[/]dc:identifier>', line)
         if match_obj:
-             ark.append(match_obj.group(1))
+            ark.append(match_obj.group(1))
     return ark
 
+def get_page(arks):
+    for ark in arks:
+        print("Retrieve ark" + ark)
+        request_url = 'http://gallica.bnf.fr/iiif/' + ark + '/manifest.json'
+        response =  requests.get(request_url)
+        if response.status_code == 200:
+            manifest = json.loads(response.content)
+            for image in manifest['sequences']:
+                url_image = image['canvases'][0]['images'][0]['resource']['@id']
+                response = requests.get(url_image)
+                with open('files/' + str(randint(0,1000)) + '.jpg', 'wb') as f:
+                    print('Save file' + ark)
+                    f.write(response.content)
+
+
 xml = get_xml_gallica()
-print get_ark(xml.content)
+print get_page(get_ark(xml.content))
