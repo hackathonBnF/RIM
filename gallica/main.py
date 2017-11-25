fde8719624864439e83af9d22719145f8c6ee38b
Nicolas Bounoughaz <nicolas.bounoughaz@scmfrance.fr>
2017-11-25 20:04:45 +0100

    save images

--- gallica/main.py
+++ gallica/main.py
@@ -6,6 +6,7 @@ from random import randint
 import json
 
 url_gallica = "http://gallica.bnf.fr/"
+url_iiif = 'http://gallica.bnf.fr/iiif/'
 
 def get_xml_gallica():
     request_url = url_gallica + "SRU?operation=searchRetrieve&exactSearch=false&collapsing=true&version=1.2&query=(dc.type%20all%20%22partition%22)&suggest=10'"
@@ -21,19 +22,21 @@ def get_ark(xml):
             ark.append(match_obj.group(1))
     return ark
 
-def get_page(arks):
+def get_partition(arks, offset = 5, limitpage = 10):
     for ark in arks:
         print("Retrieve ark" + ark)
-        request_url = 'http://gallica.bnf.fr/iiif/' + ark + '/manifest.json'
+        request_url = url_iiif + ark + '/manifest.json'
         response =  requests.get(request_url)
         if response.status_code == 200:
             manifest = json.loads(response.content)
             for image in manifest['sequences']:
-                url_image = image['canvases'][0]['images'][0]['resource']['@id']
-                response = requests.get(url_image)
-                with open('files/' + str(randint(0,1000)) + '.jpg', 'wb') as f:
-                    print('Save file' + ark)
-                    f.write(response.content)
+                for canvas in image['canvases']:
+                    for image in canvas['images']:
+                        url_image = image['resource']['@id']
+                        response = requests.get(url_image)
+                        with open('files/' + str(randint(0,1000)) + '.jpg', 'wb') as f:
+                            print('Save file' + ark)
+                            f.write(response.content)
 
 
 xml = get_xml_gallica()
