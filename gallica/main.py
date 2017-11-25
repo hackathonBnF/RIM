423fe43defeff04cb784619a74004f5490152c0a
Nicolas Bounoughaz <nicolas.bounoughaz@scmfrance.fr>
2017-11-25 22:34:59 +0100

    add query option

--- gallica/main.py
+++ gallica/main.py
@@ -4,14 +4,17 @@ import requests
 import re
 from random import randint
 import json
+import optparse
 
 url_gallica = "http://gallica.bnf.fr/"
 url_iiif = 'http://gallica.bnf.fr/iiif/'
 
-def get_xml_gallica():
-    request_url = url_gallica + "SRU?operation=searchRetrieve&exactSearch=false&collapsing=true&version=1.2&query=(dc.type%20all%20%22partition%22)&suggest=10'"
+def get_xml_gallica(query = ''):
+    request_url = url_gallica + "SRU?operation=searchRetrieve&exactSearch=false&collapsing=true&version=1.2&query=(%28gallica%20all%20\"" + query + "\"%29%20and%20dc.type%20all%20\"partition\")&suggest=10'"
     response = requests.get(request_url)
-    return response
+    if response.status_code == 200:
+        return response.content
+    return ''
 
 def get_ark(xml):
     ark = []
@@ -38,6 +41,18 @@ def get_partition(arks, offset = 5, limitpage = 10):
                             print('Save file' + ark)
                             f.write(response.content)
 
+parser = optparse.OptionParser()
 
-xml = get_xml_gallica()
-print get_page(get_ark(xml.content))
+parser.add_option('-d', '--dest',
+    action="store", dest="dest",
+    help="destination directory", default="files/")
+
+parser.add_option('-q', '--query',
+    action="store", dest="query",
+    help="Query term", default="")
+
+options, args = parser.parse_args()
+
+xml = get_xml_gallica(options.query)
+arks = get_ark(xml)
+get_partition(arks)
