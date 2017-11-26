53f612dcaa46094b24c1f854636d8dfb8f890691
Nicolas Bounoughaz <nicolas.bounoughaz@scmfrance.fr>
2017-11-26 15:47:29 +0100

    search engine

--- backend/classes/score.py
+++ backend/classes/score.py
@@ -1,9 +1,20 @@
 import mysql.connector as mariadb
 
-class score:
+class Score:
 
-    def __ini__(self, ark, cursor):
+    def __init__(self, cursor, ark = ''):
         self.ark = ark;
         self.cursor = cursor
 
-    
+    def get_all(self, limit = 50):
+        self.cursor.execute("SELECT * from glc_partition",(limit))
+        return self.cursor.fetchall()
+
+    def get_byid(self, id):
+        self.cursor.execute('SELECT * FROM glc_partition where ark = "%s"',(id))
+        return self.cursor.fetchall()
+
+    def search(self, query):
+        sql = "SELECT * FROM glc_metadata WHERE LOWER(value) like %s"
+        self.cursor.execute(sql, ("%" + query + "%",))
+        return self.cursor.fetchall()
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,26 @@
 #!bam/bin/python
 
 from flask import Flask
+from flask import jsonify
+import mysql.connector as mariadb
+from classes.score import Score
+
 app = Flask(__name__)
 
+connection = mariadb.connect(host='localhost', user='root', password='changeme', database='gallica')
+cursor = connection.cursor()
 
-
-@app.route("/score")
+@app.route("/score/all")
 def score():
-    return ""
+    score = Score(cursor)
+    return jsonify(score.get_all())
+
+@app.route("/score/<id>")
+def score_id(id):
+    score = Score(cursor)
+    return jsonify(score.get_byid(id))
+
+@app.route("/search/<q>")
+def search(q):
+    score = Score(cursor)
+    return jsonify(score.search(q))
