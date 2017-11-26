2e38f55bd7673d8391e80e0d2a73f05982ad27c6
Nicolas Bounoughaz <nicolas.bounoughaz@scmfrance.fr>
2017-11-26 14:43:43 +0100

    add score class

---
+++ backend/classes/score.py
@@ -0,0 +1,9 @@
+import mysql.connector as mariadb
+
+class score:
+
+    def __ini__(self, ark, cursor):
+        self.ark = ark;
+        self.cursor = cursor
+
+    
--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
 @app.route("/score")
 def score():
-    return "foo"
+    return ""
